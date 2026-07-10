6c96784953f01c64a46f26eb2f916ddb10f53d9b
Carlos <1000carlos.pena@gmail.com>
2026-07-10 07:38:58 -0400

    Serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -61,6 +61,7 @@ urlpatterns = [
     path('terminos/', views.terminos, name='terminos'),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
